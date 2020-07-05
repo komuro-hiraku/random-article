ffb47df295e0db637a6606117bdaca5cde624874
com4dc <h.comcom@gmail.com>
2020-07-05 23:07:51 +0900

    Add Random Endpoint and Implements
    - feat: Add endpoint
    - feat: Add Service implements

--- src/main/java/jp/classmethod/sample/repository/DmmItemRepository.java
+++ src/main/java/jp/classmethod/sample/repository/DmmItemRepository.java
@@ -3,9 +3,12 @@ package jp.classmethod.sample.repository;
 
 import jp.classmethod.sample.service.DmmItem;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface DmmItemRepository {
 
-    List<DmmItem> fetchDmmItem(int hits); // 指定のアイテム数をFetchする
+    List<DmmItem> fetchDmmItems(int hits); // 指定のアイテム数をFetchする
+
+    List<DmmItem> fetchAllItems();
 }
--- src/main/java/jp/classmethod/sample/service/DmmService.java
+++ src/main/java/jp/classmethod/sample/service/DmmService.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @Slf4j
@@ -17,9 +19,30 @@ public class DmmService {
 
     public static final int DEFAULT_ITEM_SIZE = 5;
 
-    public List<DmmItem> getDmmContents(Integer hit) {
+    /**
+     * 指定した数だけ先頭から引っ張ってくる
+     * @param hit 引っ張ってくる数
+     * @return {@link List} of {@link DmmItem}
+     */
+    public List<DmmItem> getItems(Integer hit) {
 
         var hitCount = Optional.ofNullable(hit).orElse(DEFAULT_ITEM_SIZE);
-        return repository.fetchDmmItem(hitCount);
+        return repository.fetchDmmItems(hitCount);
+    }
+
+    /**
+     * 取得したアイテムリストからランダムで一つだけ取得する
+     * @return {@link Optional} of {@link DmmItem}
+     */
+    public Optional<DmmItem> getRandomItem() {
+        var random = new Random(System.currentTimeMillis()).nextInt();
+        var items = repository.fetchAllItems();
+
+        if (items.size() > 0) {
+            var index = random % items.size();
+            return Optional.of(items.get(index));
+        } else {
+            return Optional.empty();
+        }
     }
 }
